edec1c46f7c7e1e697a2e7bc85aad46d6e0a1f12
Nahomie ND <nducasse3@gmail.com>
2020-12-04 15:05:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ function addElementToBeginningOfArray(array, element){
 return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+function destructivelyAddElementToBeginningOfArray(array, foo){
+  array.unshift(foo)
   return array
 }
 
